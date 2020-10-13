2d1387559b357fc0787c8113b257844b275c3c11
Mikko Korpela <mikko.korpela@gmail.com>
2020-10-13 23:21:02 +0300

    first draft of the logo

--- sources/src/components/AppHeader.vue
+++ sources/src/components/AppHeader.vue
@@ -2,11 +2,10 @@
       <section class="header">
         <div class="main-header-container">
           <h1 class="main-header">
-            GLOBAL<br>
-            <span class="white">2</span>TAKE<span class="white">2</span><br>
-            <span class="white">0</span>OVER<span class="white">0</span>
+            <span class="white">2</span><span>WORLD</span><span class="white">WORKSHOP</span><span>2</span>
+            <span class="white">0</span><span>WIDE</span><span  class="white">WEDNESDAY</span><span>0</span>
           </h1>
-          <h1 class="center white">14-17/JAN/2020</h1>
+          <h1 class="center white">02/DEC/2020</h1>
         </div>
       </section>
 </template>
